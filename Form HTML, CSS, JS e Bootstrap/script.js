7a9321dee53cbef6fe0585fcbb9fb700301eb23b
Michele <michelework02@gmail.com>
2026-02-20 18:23:13 +0100

    esercizio form bootstrap

---
+++ Form HTML, CSS, JS e Bootstrap/script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("#form-speciale");
+
+form.addEventListener("submit", gestisciInvioForm);
+
+function gestisciInvioForm(event) {
+    event.preventDefault();
+
+    // alert("invio form");
+}
